b93889739be3734250090b9777748d4925d3e226
Lucas Vinícius <62507586+lucasviinic@users.noreply.github.com>
2024-03-02 13:21:28 -0300

    feat: create flashcard container box

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key,required this.title});
+  const HomeScreen({super.key, required this.title});
 
   final String title;
 
@@ -10,13 +10,11 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  void _createSubject() {
+    return;
   }
+
+  final List<String> topics = ['Topic 1', 'Topic 2', 'Topic 3'];
 
   @override
   Widget build(BuildContext context) {
@@ -41,18 +39,64 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Image.asset('assets/images/logo-v1.png', height: 45, width: 45),
         centerTitle: true,
         actions: const [
-            Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Icon(Icons.search, size: 35),
-            ),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Icon(Icons.search, size: 35),
+          ),
         ],
       ),
-      drawer: const Drawer(backgroundColor: Color.fromRGBO(57, 167, 255, 1),),
+      drawer: const Drawer(
+        backgroundColor: Color.fromRGBO(57, 167, 255, 1),
+      ),
       body: Container(
         color: const Color.fromRGBO(224, 244, 255, 1),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(bottom: 10),
+                child: Divider(color: Colors.black),
+              ),
+              Container(
+                height: 70,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 180, 217, 255),
+                  borderRadius: BorderRadius.circular(10)
+                ),
+                child: Row(
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        showMenu(
+                          context: context,
+                          position: const RelativeRect.fromLTRB(0, 70, 0, 0),
+                          items: topics.map((String topic) {
+                            return PopupMenuItem<String>(
+                              value: topic,
+                              child: Text(topic),
+                            );
+                          }).toList(),
+                          elevation: 8.0,
+                        );
+                      },
+                      child: const Icon(Icons.arrow_right_rounded, size: 50),
+                    ),
+                    const Text("Biologia", style: TextStyle(fontSize: 20)),
+                    const Spacer(),
+                    const Padding(
+                      padding: EdgeInsets.all(15),
+                      child: Icon(Icons.menu),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: _createSubject,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
