94cf96d2b9ec785a21d60b29b524ae62f6081062
Lucas Vinícius <62507586+lucasviinic@users.noreply.github.com>
2024-04-05 18:54:11 -0300

    feat: add flashcard edit modal

--- lib/widgets/FlashcardPreview.dart
+++ lib/widgets/FlashcardPreview.dart
@@ -1,11 +1,18 @@
 import 'package:flashcard_forge_app/utils/constants.dart';
 import 'package:flutter/material.dart';
 
-class FlashcardPreview extends StatelessWidget {
+class FlashcardPreview extends StatefulWidget {
   final String text;
 
   const FlashcardPreview(this.text, {super.key});
 
+  @override
+  State<FlashcardPreview> createState() => _FlashcardPreviewState();
+}
+
+class _FlashcardPreviewState extends State<FlashcardPreview> {
+  List<bool> isSelected = [false, false, false];
+  
   String _getDisplayText(String text) {
     if (text.length > 30 && text.length < 40) {
       return "${text.substring(0, 27)}...";
@@ -20,59 +27,131 @@ class FlashcardPreview extends StatelessWidget {
     return showDialog<void>(
       context: context,
       builder: (BuildContext context) {
-        return AlertDialog(
-          backgroundColor: AppColors.secondaryColor,
-          title: const Text('Edit flashcard', style: TextStyle(color: AppColors.whiteColor)),
-          content: const SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text("Question:"),
-                TextField(
-                  decoration: InputDecoration(labelText: 'Enter flashcard question'),
-                  keyboardType: TextInputType.multiline,
-                  maxLines: 3,
-                  style: TextStyle(color: AppColors.whiteColor),
+        return StatefulBuilder(builder: (stfContext, stfSetState) {
+          return AlertDialog(
+            backgroundColor: AppColors.secondaryColor,
+            title: const Text('Edit flashcard', style: TextStyle(color: AppColors.whiteColor)),
+            content: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text("Question:"),
+                  const TextField(
+                    decoration: InputDecoration(labelText: 'Enter flashcard question'),
+                    keyboardType: TextInputType.multiline,
+                    maxLines: 3,
+                    style: TextStyle(color: AppColors.whiteColor),
+                  ),
+                  const SizedBox(height: 10),
+                  const Text("Answer:"),
+                  const TextField(
+                    decoration: InputDecoration(labelText: 'Enter flashcard answer'),
+                    keyboardType: TextInputType.multiline,
+                    maxLines: 3,
+                    style: TextStyle(color: AppColors.whiteColor),
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    children: [
+                      GestureDetector(
+                        onTap:() => stfSetState(() {
+                          isSelected.fillRange(0, isSelected.length, false);
+                          isSelected[0] = true;
+                        }),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: isSelected[0] ? AppColors.greenEasy : null,
+                            border: Border.all(width: 1, color: AppColors.greenEasy),
+                            borderRadius: BorderRadius.circular(50)
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                            child: Text(
+                              "Easy", 
+                              style: TextStyle(
+                                color: isSelected[0] ? AppColors.whiteColor : AppColors.greenEasy)
+                              ),
+                          ),
+                        )
+                      ),
+                      const SizedBox(width: 5),
+                      GestureDetector(
+                        onTap:() => stfSetState(() {
+                          isSelected.fillRange(0, isSelected.length, false);
+                          isSelected[1] = true;
+                        }),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: isSelected[1] ? AppColors.blueNeutral : null,
+                            border: Border.all(width: 1, color: AppColors.blueNeutral),
+                            borderRadius: BorderRadius.circular(50)
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                            child: Text(
+                              "Medium", 
+                              style: TextStyle(
+                                color: isSelected[1] ? AppColors.whiteColor : AppColors.blueNeutral)
+                              ),
+                          ),
+                        )
+                      ),
+                      const SizedBox(width: 5),
+                      GestureDetector(
+                        onTap:() => stfSetState(() {
+                          isSelected.fillRange(0, isSelected.length, false);
+                          isSelected[2] = true;
+                        }),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: isSelected[2] ? AppColors.redHard : null,
+                            border: Border.all(width: 1, color: AppColors.redHard),
+                            borderRadius: BorderRadius.circular(50)
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                            child: Text(
+                              "Hard", 
+                              style: TextStyle(
+                                color: isSelected[2] ? AppColors.whiteColor : AppColors.redHard)
+                              ),
+                          ),
+                        )
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+            actions: <Widget>[
+              TextButton(
+                style: TextButton.styleFrom(
+                  textStyle: Theme.of(context).textTheme.labelLarge,
                 ),
-                SizedBox(height: 10),
-                Text("Answer:"),
-                TextField(
-                  decoration: InputDecoration(labelText: 'Enter flashcard answer'),
-                  keyboardType: TextInputType.multiline,
-                  maxLines: 3,
-                  style: TextStyle(color: AppColors.whiteColor),
-                )
-              ],
-            ),
-          ),
-          actions: <Widget>[
-            TextButton(
-              style: TextButton.styleFrom(
-                textStyle: Theme.of(context).textTheme.labelLarge,
+                child: const Text('Save'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
               ),
-              child: const Text('Save'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-            TextButton(
-              style: TextButton.styleFrom(
-                textStyle: Theme.of(context).textTheme.labelLarge,
+              TextButton(
+                style: TextButton.styleFrom(
+                  textStyle: Theme.of(context).textTheme.labelLarge,
+                ),
+                child: const Text('Cancel'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
               ),
-              child: const Text('Cancel'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
-    );
+            ],
+          );
+        },
+      );
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    String displayText = _getDisplayText(text);
+    String displayText = _getDisplayText(widget.text);
 
     return Card(
       elevation: 4.0,
